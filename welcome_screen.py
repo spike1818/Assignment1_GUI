1896b5a92a8b4581ceea80eaaf38eb3aafb3a6c4
Alexandra Atkinson <atkina10@mcmaster.ca>
2023-09-28 17:13:41 -0400

    Alex update
    I added a splash screen it's super fun and exciting

--- welcome_screen.py
+++ welcome_screen.py
@@ -1,10 +1,23 @@
 import tkinter as tk
 from tkinter import *
-welcome = tk.Tk()
-Label(welcome, text="Username").grid(row=0)
-Label(welcome, text="Password").grid(row=1)
-entry1 = Entry(welcome)
-entry2 = Entry(welcome)
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
+from token import SLASH
+
+splash_screen = Tk()
+# splash_screen.title("Welcome")
+splash_screen.geometry("500x500")
+
+splash_label = Label(splash_screen, text="Welcome!", font=100)
+splash_label.pack()
+
+def main():
+    splash_screen.destroy()
+    welcome = tk.Tk()
+    Label(welcome, text="Username").grid(row=0)
+    Label(welcome, text="Password").grid(row=1)
+    entry1 = Entry(welcome)
+    entry2 = Entry(welcome)
+    entry1.grid(row=0, column=1)
+    entry2.grid(row=1, column=1)
+
+splash_screen.after(3000, main)
 mainloop()
