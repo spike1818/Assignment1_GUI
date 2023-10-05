c9707886f438d7607d5c0a3422b15ac48096f7e2
Alastair-Irvine <irvina6@mcmaster.ca>
2023-10-05 15:48:58 -0400

    Closing previous windows
    closes log in window when log in is successful and reopens login whens sign out is clicked

--- open_profile.py
+++ open_profile.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import *
 from tkinter import messagebox
 from AAI_screen import open_AAI
 from AOO_screen import open_AOO
@@ -18,7 +17,6 @@ def open_profile():
     welcome_message = tk.Label(profile_topframe, text = "Welcome 'name'", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
     tracing_message = tk.Label(profile_middleframe, text = "Tracing Methods", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
     sign_out = tk.Button(profile_bottomframe, text = "Sign Out", bg='#FFFFFF', fg='#000000', font=("Arial", 10), command = profile.destroy)
-    exit_program = tk.Button(profile_bottomframe, text = "Exit", bg='#FFFFFF', fg='#000000', font=("Arial", 10), command = profile.destroy)
     aoo = tk.Button(profile_middleframe, text = "AOO", bg='#FFFFFF', fg='#000000', font=("Arial", 10), command = open_AOO)
     voo = tk.Button(profile_middleframe, text = "VOO", bg='#FFFFFF', fg='#000000', font=("Arial", 10), command = open_VOO)
     aai = tk.Button(profile_middleframe, text = "AAI", bg='#FFFFFF', fg='#000000', font=("Arial", 10), command = open_AAI)
@@ -28,7 +26,6 @@ def open_profile():
 
     tracing_message.grid(row=0, column=0)
     sign_out.grid(row=0, column=0)
-    exit_program.grid(row=1, column=0)
     aoo.grid(row=1, column=0)
     voo.grid(row=2, column=0)
     aai.grid(row=3, column=0)
