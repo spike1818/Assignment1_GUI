46b8f445edd4a1512355633737a4b21f67922b1d
Alexandra Atkinson <atkina10@mcmaster.ca>
2023-10-11 12:57:08 -0400

    Revert "Update VVI_screen.py"
    This reverts commit dfbae7ad576c537f6d69aaaaf2b32142a245528d.

--- VVI_screen.py
+++ VVI_screen.py
@@ -5,7 +5,7 @@ def open_VVI():
     vvi = tk.Tk()
     vvi.geometry("500x400")
     vvi.configure(bg='#4863A0')
-    vvi.title("VVI Page")
+    vvi.title("vvi Page")
     vvi_middleframe = tk.Frame(vvi, bg = '#4863A0')
     welcome_message = tk.Label(vvi_middleframe, text = "VVI", bg='#4863A0', fg='#FFFFFF', font=("Arial", 16))
     welcome_message.grid(row = 0, column = 0)
